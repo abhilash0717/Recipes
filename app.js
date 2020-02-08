df136b728bca2f025d1cd397559e997661c95f51
abhilash0717 <abhilashabhi0717@gmail.com>
2020-02-08 19:11:48 +0000

    third commit with all the routes logic completed

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express      = require("express");
 var app          = express();
 var mongoose     = require("mongoose");
 var bodyParser   = require("body-parser");
+var methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/recipes_db", {useNewUrlParser:true,useUnifiedTopology:true});
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 //Mongoose schema 
 
@@ -55,6 +57,53 @@ app.post("/recipes", function(req,res){
 	});
 });
 
+//SHOW ROUTE 
+
+app.get("/recipes/:id", function(req,res){
+	Recipe.findById(req.params.id , function(err, foundRecipe){
+		if(err){
+			res.redirect("/recipes");
+		}else {
+			res.render("show", {foundRecipe:foundRecipe});
+		}
+	});
+});
+
+//EDIT ROUTE 
+
+app.get("/recipes/:id/edit", function(req,res){
+Recipe.findById(req.params.id, function(err, foundRecipe){
+	if(err){
+		res.redirect("/recipes");
+	}else {
+		res.render("edit", {recipe:foundRecipe});
+	}
+	});
+});
+
+//UPDATE ROUTE 
+
+app.put("/recipes/:id", function(req,res){
+	Recipe.findByIdAndUpdate(req.params.id, req.body.recipe, function(err, updatedRecipe){
+		if(err){
+			res.redirect("/recipes");
+		}else {
+			res.redirect("/recipes/" + req.params.id);
+		}
+	});
+});
+
+//DESTROY ROUTE 
+
+app.delete("/recipes/:id" , function(req,res){
+	Recipe.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+				res.redirect("/recipes");
+		} else {
+			res.redirect("/recipes");
+		}
+	});
+});
 
 app.listen(3000, function(){
 	console.log("server started");
