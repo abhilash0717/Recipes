b0f62dad7ac3be79f5fb5332c82e3594380deb27
abhilash0717 <abhilashabhi0717@gmail.com>
2020-02-08 12:57:03 +0000

    second commit create index new create routes

--- app.js
+++ app.js
@@ -1,12 +1,60 @@
-var express = require("express");
-var app     = express();
+var express      = require("express");
+var app          = express();
+var mongoose     = require("mongoose");
+var bodyParser   = require("body-parser");
+
+mongoose.connect("mongodb://localhost/recipes_db", {useNewUrlParser:true,useUnifiedTopology:true});
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
+//Mongoose schema 
+
+var recipeSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body : String,
+	created: {type:Date, default:Date.now}
+});
+
+var Recipe = mongoose.model("Recipe", recipeSchema);
+
+//Routes
 app.get("/", function(req,res){
 	res.render("landing");
 });
+
+// INDEX ROUTE 
+
+app.get("/recipes", function(req,res){
+	Recipe.find({}, function(err, foundRecipe){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("landing" , {recipes:foundRecipe});
+		}
+	});
+});
+
+//NEW ROUTE 
+
+app.get("/recipes/new" , function(req,res){
+	res.render("new");
+});
+
+//CREATE ROUTE 
+
+app.post("/recipes", function(req,res){
+	Recipe.create(req.body.recipe , function(err, newRecipe){
+		if(err){
+			res.render("new");
+		}else{
+			res.redirect("/recipes");
+		}
+	});
+});
+
 
 app.listen(3000, function(){
 	console.log("server started");
